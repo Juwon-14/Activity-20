5a743265e773cd4a4e252de0ad8200d1f4922b48
Juwon-14 <wndnjs9127@gmail.com>
2024-10-29 14:08:32 -0700

    Update main.cpp

--- Activity-20/main.cpp
+++ Activity-20/main.cpp
@@ -2,13 +2,12 @@
 //  main.cpp
 //  Activity-20
 //
-//  Created by 홍주원 on 10/14/24.
+//  Created by Juwon Hong on 10/14/24.
 //
 
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
     std::cout << "Hello, World!\n";
     return 0;
 }
