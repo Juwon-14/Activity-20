3ba0e9e6d549090cb3c94f1d9f49f3f90d589c9c
Juwon-14 <wndnjs9127@gmail.com>
2024-11-02 22:05:16 -0700

    Update main.cpp

--- Activity-20/main.cpp
+++ Activity-20/main.cpp
@@ -6,8 +6,29 @@
 //
 
 #include <iostream>
+#include "functions.h"
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    std::cout << "Hello, World!\n";
+int main() {
+    char choice;
+
+    do {
+        int size = 0;
+        double* arr = inputNumbers(size);
+
+        printSortedArray(arr, size);
+
+        double sum = calculateSum(arr, size);
+        cout << "The sum of the numbers is: " << sum << endl;
+
+        delete[] arr;
+
+        choice = askToContinue();
+        if (choice == 'n' || choice == 'N') {
+            cout << "Goodbye!" << endl;
+        }
+
+    } while (choice == 'y' || choice == 'Y');
+
     return 0;
 }
